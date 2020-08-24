5473f4badc5d25514e474d786b5f488288f8e91d
Pader <ypnow@163.com>
2020-08-24 16:25:23 +0800

    cache 使用示例

--- src/controller/example/redis.rs
+++ src/controller/example/redis.rs
@@ -20,5 +20,14 @@ pub async fn getkey(state: Data<AppState>) -> impl Responder {
 #[get("/redis/getcache")]
 pub async fn getcache(state: Data<AppState>) -> impl Responder {
     let value = state.cache.get_or_default::<String>("test", "Test World".to_string()).await;
-    value
+
+
+    let v2 = state.cache.get("test2").await;
+
+    let v2 = match v2 {
+        Some(v) => v,
+        None => "No Value".to_string()
+    };
+
+    format!("test is: {}, test2 is: {}", value, v2)
 }
